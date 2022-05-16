4235d71e6baa583718193ddabd4912e6a55e959b
NgairoTs <Ngairotap@gmail.com>
2022-05-16 16:22:22 +1200

    unicorn

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,55 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Inflate The Unicorn!")
+let unicorn = ('unicorn0', 'unicorn1', 'unicorn2')
+unicorn0 = document.getElementById('unicorn0')
+unicorn1 = document.getElementById('unicorn1')
+unicorn2 = document.getElementById('unicorn2')
+
+let level0 = 0
+let level1 = 0
+let level2 = 0
+let inflationlevels = [level0, level1, level2]
+
+unicorn0.onclick = clicked
+unicorn1.onclick = clicked
+unicorn2.onclick = clicked
+
+// me mohio ko tehea hōiho e pāwhiri  ana, I want to know which unicorn has been clicked.
+// tipu te whakamakoha kōeke, increase its inflation levels.
+// Tini i te pikitia ki te whakahua hou. change the src of the img to the new img
+
+function clicked(event) {
+  let unicorn = event.target
+  // image.src = 'images/unicorn-1.png'
+  // image.src = 'images/unicorn-2.png'
+
+  if (unicorn.id == 'unicorn0') {
+    inflationlevels[0]++
+
+    if (inflationlevels[0] == 4) inflationlevels[0] = 0
+
+    unicorn.src = 'images/unicorn-' + inflationlevels[0] + '.png'
+  }
+  if (unicorn.id == 'unicorn1') {
+    inflationlevels[1]++
+
+    if (inflationlevels[1] == 4) inflationlevels[1] = 0
+
+    unicorn.src = 'images/unicorn-' + inflationlevels[1] + '.png'
+  }
+
+  if (unicorn.id == 'unicorn2') {
+    inflationlevels[2]++
+
+    if (inflationlevels[2] == 4) inflationlevels[2] = 0
+
+    unicorn.src = 'images/unicorn-' + inflationlevels[2] + '.png'
+  }
+}
+
+console.log(unicorn0.onclick)
+//if (unicorn.id == 'unicorn1') inflationlevel1++
+
+//if (unicorn.id == 'unicorn2') inflationlevel2++
